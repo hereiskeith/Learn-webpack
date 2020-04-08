6707548242de2981cd19edda28a4fa916c4fb35a
unknown <hereiskeith@gmail.com>
2020-04-08 14:39:59 -0400

    Minify JS, CSS, and HTML in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -6,19 +5,8 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // 3. Inject styles into DOM
-          "css-loader", // 2. Turn css into commonjs
-          "sass-loader" // 1. Turn sass into css
-        ]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,22 @@
-const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: "development"
+  mode: "development",
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Inject styles into DOM
+          "css-loader", // 2. Turn css into commonjs
+          "sass-loader" // 1. Turn sass into css
+        ]
+      }
+    ]
+  }
 });
